5f5e59ab7e22c148c8848322aa17e426ab2d8069
Vishal Kumar Ojha <143877699+vishalkumarojha@users.noreply.github.com>
2024-08-12 02:51:35 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,116 @@
+import questions from './questions.js';
+
+
+// Buzzer & Chaching sounds
+var buzzer = document.getElementById("buzzer");
+buzzer = window.buzzer;
+
+function playbuzzer() {
+  buzzer.play();
+}
+
+function pausebuzzer() {
+  buzzer.pause();
+}
+var chaching = document.getElementById("chaching");
+chaching = window.chaching;
+
+function playchaching() {
+  chaching.play();
+}
+
+function pausechaching() {
+  chaching.pause();
+}
+// Global variable  score...
+var score = 0;
+//
+$(function () {
+  // Remove Element after click
+  $(".gridbtn").on("click", function () {
+    $(this).addClass("disabled");
+    $(this).remove();
+  });
+});
+
+$("#closesubmit").on("click", function () {
+  $('#myModal').modal('toggle');
+  submit();
+});
+
+// Submit Question Answer
+function submit(a, b) {
+  var selected = $(".modal-body input:checked").val();
+  // Determine if correct answer was chosen and alert prize
+  if (selected === questions[window.currentQuestion].correctAnswer) {
+    // console.log(selected + " was selected");
+    chaching.play();
+    window.addprize = questions[window.currentQuestion].cashPrize;
+    score = score + window.addprize;
+    $('#score1').html("  $" + score);
+    if (!Math.sign(score)) {
+      $('#score1').addClass("negative");
+    } else {
+      $('#score1').removeClass("negative");
+    }
+  }
+  // Determine if incorrect answer was chosen and alert prize
+  if (selected !== questions[window.currentQuestion].correctAnswer) {
+    buzzer.play();
+    window.subprize = questions[window.currentQuestion].cashPrize;
+    score = score - window.subprize;
+    console.log(score);
+    $('#score1').html("  $" + score);
+    if (Math.sign(score)) {
+      $('#score1').addClass("negative");
+    } else {
+      $('#score1').removeClass("negative");
+    }
+  }
+}
+// Get question info from array, prepare
+function getOptions(question) {
+  var $buttonDiv = $('<div id="disabled" class="btn-group" data-bs-toggle="buttons"></div><br>');
+  question.options.forEach(function (opt) {
+    var $div = $('<div class="radio">');
+    var $label = $('<label class="radio-inline"></label');
+    var $input = $('<input type="radio" name="opts" value="' + opt + '">');
+    $label.append($input);
+    $label.append(opt);
+    $div.append($label);
+    $buttonDiv.append($div);
+  });
+  return $buttonDiv;
+}
+// Populate modal window with specific question
+function showQuestion(event, $modal) {
+  var button = $(event.relatedTarget); // Button that triggered the modal
+  var num = parseInt(button.data('num'));
+  var question = questions[num];
+  window.currentQuestion = num;
+  $modal.find('.modal-title').text(question.prompt);
+  $modal.find('.modal-body').empty().append(getOptions(question));
+}
+// Modal show/close functions
+$(function () {
+  $("#myModal").on('show.bs.modal', function (event) {
+    showQuestion(event, $(this));
+  });
+});
+$("#myModal").on('hidden.bs.modal', function () {
+  console.log('The modal is now hidden.');
+});
+// $('.modal-body input:checked').on(function () {
+//   $('#myModal').modal('toggle');
+//   return false;
+// });
+
+function updateScores() {
+  const player1Score = document.getElementById('player1').value;
+  const player2Score = document.getElementById('player2').value;
+  const player3Score = document.getElementById('player3').value;
+
+  console.log(`Player 1: ${player1Score}`);
+  console.log(`Player 2: ${player2Score}`);
+  console.log(`Player 3: ${player3Score}`);
+}
---
+++ js/questions.js
@@ -0,0 +1,159 @@
+const questions = [
+  /* Category 1 Questions */
+  {
+    prompt: 'Which company recently introduced Vision Pro, a mixed-reality headset ?',
+    options: ['Done'],
+    correctAnswer: 'Done',
+    cashPrize: 100
+  },
+  {
+    prompt: 'What is the name of the AI-powered search engine launched by Google in 2023 ?',
+    options: ['Done'],
+    correctAnswer: 'Done',
+    cashPrize: 250
+  },
+  {
+    prompt: ' What is the name of the new framework introduced by Meta to help build AI models more efficiently ?',
+    options: ['Done'],
+    correctAnswer: 'Done',
+    cashPrize: 500
+  },
+  {
+    prompt: ' Which company recently reached a $3 trillion market valuation in 2024 ?',
+    options: ['Done'],
+    correctAnswer: 'Done',
+    cashPrize: 750
+  },
+  {
+    prompt: ' Which company announced the world first commercial quantum computer in 2024 ? ',
+    options: ['Done'],
+    correctAnswer: 'Done',
+    cashPrize: 1000
+  },
+  /* Category 2 Questions */
+  {
+    prompt: ' Which startup is known for its rapid rise by offering instant grocery delivery and recently faced a market exit in 2024? ',
+    options: ['Done'],
+    correctAnswer: 'Done',
+    cashPrize: 100
+  },
+  {
+    prompt: ' What is the name of the startup that became the fastest unicorn in 2023, reaching a $1 billion valuation within just two months of launching ?',
+    options: ['Done'],
+    correctAnswer: 'Done',
+    cashPrize: 250
+  },
+  {
+    prompt: ' Which startup, focused on carbon capture technology, was acquired by Stripe in 2024 ?',
+    options: ['Done'],
+    correctAnswer: 'Done',
+    cashPrize: 500
+  },
+  {
+    prompt: ' Which 2024 startup is leading the way in autonomous drone delivery, partnering with Walmart ? ',
+    options: ['Done'],
+    correctAnswer: 'Done',
+    cashPrize: 750
+  },
+  {
+    prompt: ' Which AI-focused startup recently secured a $500 million investment led by NVIDIA in 2024 ? ',
+    options: ['Done'],
+    correctAnswer: 'Done',
+    cashPrize: 1000
+  },
+  /* Category 3 Questions */
+  {
+    prompt: ' Which fast-food chain surpassed $5 billion in digital sales in 2023 ? ',
+    options: ['Done'],
+    correctAnswer: 'Done',
+    cashPrize: 100
+  },
+  {
+    prompt: ' Which streaming platform raised its prices for the third time in two years in 2024 ?',
+    options: ['Done'],
+    correctAnswer: 'Done',
+    cashPrize: 250
+  },
+  {
+    prompt: ' Which social media company rebranded to X in 2023 ? ',
+    options: ['Done'],
+    correctAnswer: 'Done',
+    cashPrize: 500
+  },
+  {
+    prompt: ' Which e-commerce giant announced a new "Buy with Prime" feature to compete with Shopify in 2024? ',
+    options: ['Done'],
+    correctAnswer: 'Done',
+    cashPrize: 750
+  },
+  {
+    prompt: ' Which company became the largest producer of electric vehicles in Europe in 2024 ?',
+    options: ['Done'],
+    correctAnswer: 'Done',
+    cashPrize: 1000
+  },
+  /* Category 4 Questions */
+  {
+    prompt: ' In Python, what function would you use to get the length of a list ?',
+    options: ['Done'],
+    correctAnswer: 'Done',
+    cashPrize: 100
+  },
+  {
+    prompt: ' What does HTML stand for ?',
+    options: ['Done'],
+    correctAnswer: 'Done',
+    cashPrize: 250
+  },
+  {
+    prompt: ' Which language introduced “pattern matching” as a feature in its 2023 version ?',
+    options: ['Done'],
+    correctAnswer: 'Done',
+    cashPrize: 500
+  },
+  {
+    prompt: ' In JavaScript, what is the output of typeof null ?',
+    options: ['Done'],
+    correctAnswer: 'Done',
+    cashPrize: 750
+  },
+  {
+    prompt: ' Which programming language was developed by Mozilla for systems programming and released version 1.70 in 2024 ?',
+    options: ['Done'],
+    correctAnswer: 'Done',
+    cashPrize: 1000
+  },
+  /* Category 5 Questions */
+  {
+    prompt: ' Which company developed the AI model known as ChatGPT ?',
+    options: ['Done'],
+    correctAnswer: 'Done',
+    cashPrize: 100
+  },
+  {
+    prompt: ' What is the name of the AI image generator released by Stability AI in 2024 ?',
+    options: ['Done'],
+    correctAnswer: 'Done',
+    cashPrize: 250
+  },
+  {
+    prompt: ' Which AI model by Anthropic is known for emphasizing safety and ethical considerations ?',
+    options: ['Done'],
+    correctAnswer: 'Done',
+    cashPrize: 500
+  },
+  {
+    prompt: ' Which 2024 AI tool by Meta can generate realistic human avatars for virtual reality applications ? ',
+    options: ['Done'],
+    correctAnswer: 'Done',
+    cashPrize: 750
+  },
+  {
+    prompt: ' What AI principle focuses on creating AI systems that are safe and beneficial for humanity, often contrasted with accelerationism ?',
+    options: ['Done'],
+    correctAnswer: 'Done',
+    cashPrize: 1000
+  }
+];
+
+export default questions;
